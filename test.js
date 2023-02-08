f47b0e4d362fae1bfedf6e1f527d43dc12b8e33b
sophy1 <jihye.sophy.won@gmail.com>
2023-02-08 16:52:01 +0900

    Update test.js

--- test.js
+++ test.js
@@ -8,3 +8,17 @@ class Dog extends Animal {
     super.doSomething();
   }
 }
+function f(){
+  i = 1;         // Noncompliant; i is global
+
+  for (j = 0; j < array.length; j++) {  
+  }
+}
+
+
+function f(){
+  i = 1;         // Noncompliant; i is global
+
+  for (j = 0; j < array.length; j++) {  
+  }
+}
